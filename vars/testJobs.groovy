df4ba4db6e2ca779ef85ef2351b57e6d4fe0bbe4
Vic Wu <madcocomo@gmail.com>
2019-08-20 23:38:47 +0200

    fix expect

--- vars/testJobs.groovy
+++ vars/testJobs.groovy
@@ -22,7 +22,7 @@ def getTags() {
 }
 
 def verify(expect) {
-    sh "echo ${expect} > expect"
+    sh "echo '${expect}' > expect"
     sh 'java -jar target/*.jar > result'
     sh 'diff expect result'
 }
@@ -52,9 +52,7 @@ def tests() {
     return [
 [
 name: 'challenge1', 
-expect:'''
-1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, FizzBuzz, 31, 32, Fizz, 34, Buzz, Fizz, 37, 38, Fizz, Buzz, 41, Fizz, 43, 44, FizzBuzz, 46, 47, Fizz, 49, Buzz, Fizz, 52, 53, Fizz, Buzz, 56, Fizz, 58, 59, FizzBuzz, 61, 62, Fizz, 64, Buzz, Fizz, 67, 68, Fizz, Buzz, 71, Fizz, 73, 74, FizzBuzz, 76, 77, Fizz, 79, Buzz, Fizz, 82, 83, Fizz, Buzz, 86, Fizz, 88, 89, FizzBuzz, 91, 92, Fizz, 94, Buzz, Fizz, 97, 98, Fizz, 100
-''',
+expect:'''1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, FizzBuzz, 31, 32, Fizz, 34, Buzz, Fizz, 37, 38, Fizz, Buzz, 41, Fizz, 43, 44, FizzBuzz, 46, 47, Fizz, 49, Buzz, Fizz, 52, 53, Fizz, Buzz, 56, Fizz, 58, 59, FizzBuzz, 61, 62, Fizz, 64, Buzz, Fizz, 67, 68, Fizz, Buzz, 71, Fizz, 73, 74, FizzBuzz, 76, 77, Fizz, 79, Buzz, Fizz, 82, 83, Fizz, Buzz, 86, Fizz, 88, 89, FizzBuzz, 91, 92, Fizz, 94, Buzz, Fizz, 97, 98, Fizz, 100''',
 nextDesc:'''
 #### Challenge 2
 Add a line wrap at every 14 characters.
@@ -69,8 +67,7 @@ Sample output:
 ],
 [
 name: 'challenge2', 
-expect:'''
-1, 2, Fizz, 4,
+expect:'''1, 2, Fizz, 4,
  Buzz, Fizz, 7
 , 8, Fizz, Buz
 z, 11, Fizz, 1
@@ -106,8 +103,7 @@ z, Fizz, 82, 8
  91, 92, Fizz,
  94, Buzz, Fiz
 z, 97, 98, Fiz
-z, 100
-''',
+z, 100''',
 nextDesc:'''
 #### Challenge 3
 Print the first 100 numbers from Fibonacci list with FizzBuzz rule and line breaks.
@@ -126,86 +122,78 @@ Sample output:
 ],
 [
 name: 'challenge3', 
-expect:'''
-1, 1, 2, "Fizz
-", "Buzz", 8, 
-13, "Fizz", 34
-, "Buzz", 89, 
-"Fizz", 233, 3
-77, "Buzz", "F
-izz", 1597, 25
-84, 4181, "Fiz
-zBuzz", 10946,
- 17711, 28657,
- "Fizz", "Buzz
-", 121393, 196
-418, "Fizz", 5
-14229, "Buzz",
- 1346269, "Fiz
-z", 3524578, 5
-702887, "Buzz"
-, "Fizz", 2415
-7817, 39088169
-, 63245986, "F
-izzBuzz", 1655
-80141, 2679142
-96, 433494437,
- "Fizz", "Buzz
-", 1836311903,
- 2971215073, "
-Fizz", 7778742
-049, "Buzz", 2
-0365011074, "F
-izz", 53316291
+expect:'''1, 1, 2, Fizz,
+ Buzz, 8, 13, 
+Fizz, 34, Buzz
+, 89, Fizz, 23
+3, 377, Buzz, 
+Fizz, 1597, 25
+84, 4181, Fizz
+Buzz, 10946, 1
+7711, 28657, F
+izz, Buzz, 121
+393, 196418, F
+izz, 514229, B
+uzz, 1346269, 
+Fizz, 3524578,
+ 5702887, Buzz
+, Fizz, 241578
+17, 39088169, 
+63245986, Fizz
+Buzz, 16558014
+1, 267914296, 
+433494437, Fiz
+z, Buzz, 18363
+11903, 2971215
+073, Fizz, 777
+8742049, Buzz,
+ 20365011074, 
+Fizz, 53316291
 173, 862675712
-72, "Buzz", "F
-izz", 36543529
-6162, 59128672
-9879, 95672202
-6041, "FizzBuz
-z", 2504730781
-961, 405273953
-7881, 65574703
-19842, "Fizz",
- "Buzz", 27777
-890035288, 449
-45570212853, "
-Fizz", 1176690
-30460994, "Buz
-z", 3080615211
-70129, "Fizz",
- 8065155330493
-93, 1304969544
-928657, "Buzz"
-, "Fizz", 5527
-939700884757, 
-89443943237914
-64, "FizzBuzz"
-, 234167283484
-67684, 3788906
-2373143900, "F
-izz", 99194853
-094755490, 160
-50064381636707
-0, "Buzz", 420
-19614072748966
-0, "Fizz", 110
+72, Buzz, Fizz
+, 365435296162
+, 591286729879
+, 956722026041
+, FizzBuzz, 25
+04730781961, 4
+052739537881, 
+6557470319842,
+ Fizz, Buzz, 2
+7777890035288,
+ 4494557021285
+3, Fizz, 11766
+9030460994, Bu
+zz, 3080615211
+70129, Fizz, 8
+06515533049393
+, 130496954492
+8657, Buzz, Fi
+zz, 5527939700
+884757, 894439
+4323791464, Fi
+zzBuzz, 234167
+28348467684, 3
+78890623731439
+00, Fizz, 9919
+4853094755490,
+ 1605006438163
+67070, Buzz, 4
+20196140727489
+660, Fizz, 110
 00877783661019
-00, "Fizz", "B
-uzz", "Fizz", 
-75401138047463
-46000, 1220016
-0415121877000,
- 1974027421986
-8226000, "Fizz
-Buzz", 5168070
-8854858330000,
- 8362114348984
-8430000, 13530
-18523447067600
-00, "Fizz", "F
-izz"
-''',
+00, Fizz, Buzz
+, Fizz, 754011
+3804746346000,
+ 1220016041512
+1877000, 19740
+27421986822600
+0, FizzBuzz, 5
+16807088548583
+30000, 8362114
+3489848430000,
+ 1353018523447
+06760000, Fizz
+, Fizz''',
 nextDesc: '''
 #### Well Done
 '''
