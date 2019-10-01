baf356275d6a218feaaf83ca63ef68dbcae0f502
Vic Wu <madcocomo@gmail.com>
2019-10-01 23:11:37 +0800

    fix testcase error
    Revert "fix testcase error"
    
    This reverts commit ba9c5c0c01987af4726bc77f46bc61434d805c5d.

--- vars/testcases.groovy
+++ vars/testcases.groovy
@@ -17,6 +17,61 @@ Sample output:
 ],
 [
 name: 'challenge2', 
+expect:'''1, 2, Fizz, 4,
+ Buzz, Fizz, 7
+, 8, Fizz, Buz
+z, 11, Fizz, 1
+3, 14, FizzBuz
+z, 16, 17, Fiz
+z, 19, Buzz, F
+izz, 22, 23, F
+izz, Buzz, 26,
+ Fizz, 28, 29,
+ FizzBuzz, 31,
+ 32, Fizz, 34,
+ Buzz, Fizz, 3
+7, 38, Fizz, B
+uzz, 41, Fizz,
+ 43, 44, FizzB
+uzz, 46, 47, F
+izz, 49, Buzz,
+ Fizz, 52, 53,
+ Fizz, Buzz, 5
+6, Fizz, 58, 5
+9, FizzBuzz, 6
+1, 62, Fizz, 6
+4, Buzz, Fizz,
+ 67, 68, Fizz,
+ Buzz, 71, Fiz
+z, 73, 74, Fiz
+zBuzz, 76, 77,
+ Fizz, 79, Buz
+z, Fizz, 82, 8
+3, Fizz, Buzz,
+ 86, Fizz, 88,
+ 89, FizzBuzz,
+ 91, 92, Fizz,
+ 94, Buzz, Fiz
+z, 97, 98, Fiz
+z, Buzz''',
+nextDesc:'''
+#### Challenge 3
+Print the first 100 numbers from Fibonacci list with FizzBuzz rule and line breaks.
+The Fibonacci list is:
+```
+    1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...
+```
+Sample output:
+```
+1, 1, 2, Fizz,
+ Buzz, 8, 13, 
+Fizz, 34, Buzz
+...
+```
+''' 
+],
+[
+name: 'challenge3', 
 expect:'''1, 1, 2, Fizz,
  Buzz, 8, 13, 
 Fizz, 34, Buzz
@@ -90,96 +145,6 @@ Fizz, Buzz, 42
 9, 21892299583
 4555169026, Fi
 zzBuzz''',
-nextDesc:'''
-#### Challenge 3
-Print the first 100 numbers from Fibonacci list with FizzBuzz rule and line breaks.
-The Fibonacci list is:
-```
-    1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...
-```
-Sample output:
-```
-1, 1, 2, Fizz,
- Buzz, 8, 13, 
-Fizz, 34, Buzz
-...
-```
-''' 
-],
-[
-name: 'challenge3', 
-expect:'''1, 1, 2, Fizz,
- Buzz, 8, 13, 
-Fizz, 34, Buzz
-, 89, Fizz, 23
-3, 377, Buzz, 
-Fizz, 1597, 25
-84, 4181, Fizz
-Buzz, 10946, 1
-7711, 28657, F
-izz, Buzz, 121
-393, 196418, F
-izz, 514229, B
-uzz, 1346269, 
-Fizz, 3524578,
- 5702887, Buzz
-, Fizz, 241578
-17, 39088169, 
-63245986, Fizz
-Buzz, 16558014
-1, 267914296, 
-433494437, Fiz
-z, Buzz, 18363
-11903, 2971215
-073, Fizz, 777
-8742049, Buzz,
- 20365011074, 
-Fizz, 53316291
-173, 862675712
-72, Buzz, Fizz
-, 365435296162
-, 591286729879
-, 956722026041
-, FizzBuzz, 25
-04730781961, 4
-052739537881, 
-6557470319842,
- Fizz, Buzz, 2
-7777890035288,
- 4494557021285
-3, Fizz, 11766
-9030460994, Bu
-zz, 3080615211
-70129, Fizz, 8
-06515533049393
-, 130496954492
-8657, Buzz, Fi
-zz, 5527939700
-884757, 894439
-4323791464, Fi
-zzBuzz, 234167
-28348467684, 3
-78890623731439
-00, Fizz, 9919
-4853094755490,
- 1605006438163
-67070, Buzz, 4
-20196140727489
-660, Fizz, 110
-00877783661019
-00, Fizz, Buzz
-, Fizz, 754011
-3804746346000,
- 1220016041512
-1877000, 19740
-27421986822600
-0, FizzBuzz, 5
-16807088548583
-30000, 8362114
-3489848430000,
- 1353018523447
-06760000, Fizz
-, Fizz''',
 nextDesc: '''
 #### Challenge 4
 Accept command line argument `--fibonacci` to use Fibonacci list, otherwise use normal numbers.
