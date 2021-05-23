957fd77548164ea8631aa3620818298a27b77f1c
Vic Wu <madcocomo@gmail.com>
2021-05-23 11:11:10 +0800

    remove ut files to avoid confilic

--- vars/testcases.groovy
+++ vars/testcases.groovy
@@ -2,26 +2,26 @@ def supermarket() {
     return [
 [
 name: 'story1', 
-command: 'git checkout --ignore-other-worktrees remotes/origin/at -- src/test/acceptance-test src/test/resources/acceptance-test src/test/java/cucumber; ./gradlew clean test -Pat=story1'
+command: 'rm -r src/test/java; git checkout --ignore-other-worktrees remotes/origin/at -- src/test/acceptance-test src/test/resources/acceptance-test src/test/java/cucumber; ./gradlew clean test -Pat=story1'
 ],
 [
 name: 'story2', 
-command: 'git checkout --ignore-other-worktrees remotes/origin/at -- src/test/acceptance-test src/test/resources/acceptance-test src/test/java/cucumber; ./gradlew clean test -Pat=story2'
+command: 'rm -r src/test/java; git checkout --ignore-other-worktrees remotes/origin/at -- src/test/acceptance-test src/test/resources/acceptance-test src/test/java/cucumber; ./gradlew clean test -Pat=story2'
 ],
 [
 name: 'story3', 
-command: 'git checkout --ignore-other-worktrees remotes/origin/at -- src/test/acceptance-test src/test/resources/acceptance-test src/test/java/cucumber; ./gradlew clean test -Pat=story3'
+command: 'rm -r src/test/java; git checkout --ignore-other-worktrees remotes/origin/at -- src/test/acceptance-test src/test/resources/acceptance-test src/test/java/cucumber; ./gradlew clean test -Pat=story3'
 ],
 [
 name: 'story4', 
-command: 'git checkout --ignore-other-worktrees remotes/origin/at -- src/test/acceptance-test src/test/resources/acceptance-test src/test/java/cucumber; ./gradlew clean test -Pat=story4'
+command: 'rm -r src/test/java; git checkout --ignore-other-worktrees remotes/origin/at -- src/test/acceptance-test src/test/resources/acceptance-test src/test/java/cucumber; ./gradlew clean test -Pat=story4'
 ],
 [
 name: 'story5', 
-command: 'git checkout --ignore-other-worktrees remotes/origin/at -- src/test/acceptance-test src/test/resources/acceptance-test src/test/java/cucumber; ./gradlew clean test -Pat=story5'
+command: 'rm -r src/test/java; git checkout --ignore-other-worktrees remotes/origin/at -- src/test/acceptance-test src/test/resources/acceptance-test src/test/java/cucumber; ./gradlew clean test -Pat=story5'
 ],
 [
 name: 'story6', 
-command: 'git checkout --ignore-other-worktrees remotes/origin/at -- src/test/acceptance-test src/test/resources/acceptance-test src/test/java/cucumber; ./gradlew clean test -Pat=story6'
+command: 'rm -r src/test/java; git checkout --ignore-other-worktrees remotes/origin/at -- src/test/acceptance-test src/test/resources/acceptance-test src/test/java/cucumber; ./gradlew clean test -Pat=story6'
 ]
 ]}
